4b62f2ba962d6b4c38651cbbb0f198f3ef92d661
bagarang <110575081+bagarang@users.noreply.github.com>
2026-01-09 10:48:44 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,8 @@
 const config = {
-    apiUrl: "https://script.google.com/macros/s/AKfycbzs5z8F9WJD6K-kuBcuL51F6TAK-1fYe3qedrFCjh-9L2rd9S4h-rXVsZZoXU0gJvv7sA/exec"
+    apiUrl: "https://script.google.com/macros/s/AKfycbzaQyabjDiHA691x5DxR3BJ0HlEezZu5f_nac96goHsamlAw6Qs8X8fG2dAAMdvfsvlVQ/exec"
 
 };
+
 
 
 
